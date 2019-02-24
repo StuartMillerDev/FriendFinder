9c23e8da8576db9e297cb7ae8babac47e53484cb
johndoeadams <johndoeadams@gmail.com>
2019-02-24 12:14:13 -0500

    adding some psudocode

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,36 +1,73 @@
 console.log("Loaded friends.js");
 
+// This function compares an array to the other objects
+function compareAll(person){
+  var temp=person.scores;
+  var difference=[];
+  var bestMatches=[];
+  // Loop through each friend in the friends array
+  friends.forEach(function(item){
+    var scores=item.scores;
+    // loop through each score in side the current friend
+    scores.forEach(function(score){
+      // compare each number at the current score
+      var a=score;
+      var b=temp.score;
+      var c=Math.abs(a-b);
+      difference.push(c);
+    });
+    // Math for matching best set of scores. the average of the absolute difference between scores should be as low as possible.
+    // A perfect match will have almost no difference and average the same as the user.
+    if(difference.length==10){
+    bestMatches.push(calculateAverage(difference));
+    }
+
+  });
+}
+
+function calculateAverage(arr){
+  var sum=0;
+  arr.forEach(function(num){
+    sum+=num;
+  });
+  var avg=sum/difference.length;
+  return avg;
+}
+
+function addOne(person){
+  friends.push(person);
+}
 
 var friends=[
   {
   "name":"Ahmed",
   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
   "scores":[
-      5,
       1,
-      4,
+      2,
+      3,
       4,
       5,
       1,
       2,
-      5,
+      3,
       4,
-      1
+      5
     ]
 },
 {
 "name":"Rick",
 "photo":"https://upload.wikimedia.org/wikipedia/en/e/e9/Rick_and_Morty_characters_-_Rick_Sanchez.jpg",
 "scores":[
-    5,
-    1,
-    4,
-    4,
-    5,
     1,
     2,
+    3,
+    4,
+    5,
     5,
     4,
+    3,
+    2,
     1
   ]
 },
@@ -38,16 +75,16 @@ var friends=[
 "name":"Morty",
 "photo":"https://upload.wikimedia.org/wikipedia/en/d/d7/Rick_and_Morty_characters_-_Morty_Smith.jpg",
 "scores":[
-    5,
-    1,
-    4,
-    4,
-    5,
-    1,
-    2,
-    5,
-    4,
-    1
+    3,
+    3,
+    3,
+    3,
+    3,
+    3,
+    3,
+    3,
+    3,
+    3
   ]
 }
 
--- app/public/client.js
+++ app/public/client.js
@@ -1,17 +1,18 @@
-// $("#submitSurvey").click(function(){
-// var meh=[];
-// // $(".custome-select").each(function(){
-// //   var temp=$(this).val();
-// //   meh.push(temp);
-// //   });
-//   console.log("SUBMIT!!!");
-//
-// });
-
-
-$(".btn btn-primary").click(function(){
-  console.log("button pressed");
+$("#submitSurvey").click(function(){
+var meh=[];
+// collect each item from the drop down menus
+// $(".custome-select").each(function(){
+//   var temp=$(this).val();
+//   meh.push(temp);
+//   });
+  console.log("SUBMIT!!!");
+ //post request to the server passing the array meh (your answers)
 });
+
+
+// $(".btn btn-primary").click(function(){
+//   console.log("button pressed");
+// });
 
 console.log("Loaded client.js");
 // HERE
